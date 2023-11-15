d8196572a31b45d0c605d654c33b1f66795896a2
muhammetyild1z <muhammety.yildizz@gmail.com>
2023-11-15 14:40:28 +0300

    .

---
+++ FastFoodSignalR/SignalRAPI/Controllers/AboutController.cs
@@ -0,0 +1,49 @@
+﻿using FastFoodSignalR.BusinessLayer.Abstract;
+using FastFoodSignalR.DtoLayer.AboutDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRAPI.Controllers
+{
+    public class AboutController : Controller
+    {
+
+        private readonly IAboutService _aboutService;
+
+        public AboutController(IAboutService aboutService)
+        {
+            _aboutService = aboutService;
+        }
+        [HttpGet]
+        public IActionResult AboutList()
+        {
+            var values = _aboutService.TGetListAll();
+            return Ok(values);
+        }
+        [HttpPost]
+        public IActionResult CreateAbout(CreateAboutDto createAboutDto)
+        {
+            _aboutService.TAdd(_aboutService);
+            return Ok("Ekleme Basarili..");
+        }
+        [HttpPut]
+        public IActionResult UpdateAbout(int id , UpdateAboutDto updateAboutDto)
+        {
+            var value=_aboutService.TGetById(id);
+            _aboutService.Update(updateAboutDto, value);
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteAbout(int id)
+        { 
+            var value= _aboutService.TGetById(id);
+            _aboutService.TDelete(value);
+            return Ok("Silme Basarili");
+        }
+        [HttpGet("GetByIdAbout")]
+        public IActionResult GetByIdAbout(int id)
+        {
+            var value=_aboutService.TGetById(id);
+            return Ok(value);
+        }
+    }
+}
---
+++ FastFoodSignalR/SignalRAPI/Controllers/BookingController.cs
@@ -0,0 +1,47 @@
+﻿using FastFoodSignalR.BusinessLayer.Abstract;
+using FastFoodSignalR.DtoLayer.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRAPI.Controllers
+{
+    public class BookingController : Controller
+    {
+        private readonly IBookingService _bookingservice;
+
+        public BookingController(IBookingService bookingservice)
+        {
+            _bookingservice = bookingservice;
+        }
+        [HttpGet]
+        public IActionResult GetBooking()
+        {
+            var value = _bookingservice.TGetListAll();
+            return  Ok(value);
+        }
+        [HttpPost]
+        public IActionResult CreateBooking(CreateBookingDto createBookingDto)
+        {
+            _bookingservice.TAdd(createBookingDto);
+            return Ok("Rezervasyon Eklendi");
+        }
+        [HttpPut]
+        public IActionResult UpdateBooking(int id,UpdateBookingDto updateBookingDto) 
+        {
+            var value=_bookingservice.TGetById(id);
+            _bookingservice.Update(updateBookingDto, value);
+            return Ok("Rezervasyon Guncellendi");
+        }
+        [HttpDelete]
+        public IActionResult DeleteBooking(int id)
+        {
+            _bookingservice.TDelete(_bookingservice.TGetById(id));  
+            return Ok("silme basarili");
+        }
+        [HttpGet("GetByIdBooking")]
+        public IActionResult GetByIdBooking(int id)
+        {
+           var value= _bookingservice.TGetById(id);
+            return Ok(value);
+        }
+    }
+}
